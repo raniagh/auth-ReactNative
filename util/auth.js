e38bfb454d63ae165bb6f6117b724cecd576ea3d
Your Name <rghzaiel@ggn-group.com>
2023-06-21 15:31:21 +0100

    add signup and login backend

---
+++ util/auth.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const API_KEY = "AIzaSyAiKwDHGP8yNdytq3Sa-lGOZ0WDQ2dZ6E4";
+
+//Shared function between login and signup
+async function authenticate(mode, email, password) {
+  const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
+  const response = await axios.post(url, {
+    email: email,
+    password: password,
+    returnSecureToken: true,
+  });
+}
+
+export async function createUser(email, password) {
+  await authenticate("signUp", email, password);
+}
+
+export async function login(email, password) {
+  await authenticate("signInWithPassword", email, password);
+}
